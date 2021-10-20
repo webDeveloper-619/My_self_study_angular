857abe48687b3b7da9b57ca0f94fe1753615ecea
Top-coder-1 <intelipury@gmail.com>
2021-10-20 09:29:51 +0300

    add: first

---
+++ hack.sh
@@ -0,0 +1,33 @@
+from datetime import 
+for Y in {2015..2015}
+do
+  mkdir $Y
+  cd $Y
+  for M in {04..04}
+  do
+    mkdir $M
+    cd $M
+    for D in {2..3}
+    do
+      
+      mkdir $D
+      cd $D
+      for i in $(seq 1 $((1 + RANDOM % 2))) 
+      do
+        echo "$i on $M/$D/$Y" > commit.md
+        export GIT_COMMITTER_DATE="$Y-$M-$D 12:$i:00"
+        export GIT_AUTHOR_DATE="$Y-$M-$D 12:$i:00"
+        git add commit.md -f
+        git commit --date="$Y-$M-$D 12:0$i:00" -m "$i on $M $D $Y"
+      done
+      cd ../
+    done
+    cd ../
+  done
+  cd ../
+done
+git push origin master
+git rm -rf 20**
+git rm -rf 19**
+git commit -am "cleanup"
+git push origin master
